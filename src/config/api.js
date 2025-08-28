3ca2abb57f57b40720bf4b94ef6767f7dccab6f3
Jwrme <jeromemanuel2824@gmail.com>
2025-08-29 04:16:30 +0800

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,7 @@
 const API_BASE_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://fist-gym-website.onrender.com'
+  ? 'https://fistgym-v2.onrender.com'
   : 'http://localhost:3001';
 
 export { API_BASE_URL };
+
+
